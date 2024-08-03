9574b162283797e702b6b7a5ce5215a64bc87473
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-08-03 09:33:43 -0600

    Add: initialize socket io server, install cors and types cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
 import express from 'express';
-import http from 'http';
+import http from 'node:http';
+import cors from 'cors'
+import { Server } from 'socket.io';
 
 const port = 8080
 const app = express()
-const server = http.createServer(app)
+app.use(cors())
+const httpServer = http.createServer(app)
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  }
+})
 
-server.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('A user connected')
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected')
+  })
+})
+
+httpServer.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
