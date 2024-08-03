a7f3fdfcb4b290bd91914175199b055df5f452ac
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-08-03 10:31:46 -0600

    Add logic to return a room id when the room is created and add constants

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const ROOM_CREATED_EVENT = 'room-created';
+export const CREATE_ROOM_EVENT = 'create-room';
+export const JOIN_ROOM_EVENT = 'join-room';
--- src/room/index.ts
+++ src/room/index.ts
@@ -1,13 +1,17 @@
 import { Socket } from "socket.io";
+import crypto from "crypto";
+import { CREATE_ROOM_EVENT, JOIN_ROOM_EVENT, ROOM_CREATED_EVENT } from "../constants";
 
 export const roomHandler = (socket: Socket) => {
   const createRoom = () => {
+    const roomId = crypto.randomUUID();
+    socket.emit(ROOM_CREATED_EVENT, { roomId });
     console.log("user created a room");
   }
   const joinRoom = () => {
     console.log("user joined to the room");
   }
 
-  socket.on("create-room", createRoom);
-  socket.on("join-room", joinRoom);
+  socket.on(CREATE_ROOM_EVENT, createRoom);
+  socket.on(JOIN_ROOM_EVENT, joinRoom);
 }
