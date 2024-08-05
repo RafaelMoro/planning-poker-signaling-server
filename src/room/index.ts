59869327d78940e8041dae1930aea03e02e96fa6
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-08-04 23:57:09 -0600

    Add register user event

--- src/room/index.ts
+++ src/room/index.ts
@@ -1,5 +1,5 @@
 import { Socket } from "socket.io";
-import { CREATE_ROOM_EVENT, JOIN_ROOM_EVENT, MESSAGES_EVENT, RECEIVE_MESSAGE_EVENT, ROOM_CREATED_EVENT, SEND_MESSAGE_EVENT } from "../constants";
+import { CREATE_ROOM_EVENT, MESSAGES_EVENT, REGISTER_USER_EVENT, ROOM_CREATED_EVENT } from "../constants";
 
 interface CreateRoomPayload {
   userName: string;
@@ -18,26 +18,14 @@ export const roomHandler = (socket: Socket) => {
     console.log(`User ${userName} created a room`);
   }
 
-  const joinRoom = async ({ roomId, newUser }: { roomId: string, newUser: User }) => {
-    socket.emit(ROOM_CREATED_EVENT, { roomId, newUser });
-  }
-
-  const handleSendMessage = ({ user, message }: { user: string, message: string }) => {
-    console.log({ user, message})
-    socket.emit(RECEIVE_MESSAGE_EVENT, { user, message });
-  }
-
   socket.on(CREATE_ROOM_EVENT, createRoom);
-  socket.on(JOIN_ROOM_EVENT, joinRoom);
-  socket.on('123-456-send', ({ user }: { user: User }) => {
-    socket.emit('123-456-receive', { user });
-    // socket.
-  })
-  socket.on(SEND_MESSAGE_EVENT, handleSendMessage)
   socket.on('salute', (salute: string) => {
     console.log('new message', salute)
     // Necesitaba broadcast para enviar mensajes a todos los usuarios
     socket.broadcast.emit(MESSAGES_EVENT, salute)
   })
-  // socket.on(ROOM_CREATED_EVENT, handleRoom);
+  
+  socket.on(REGISTER_USER_EVENT, ({ userName }: { userName: string }) => {
+    (socket as any).userName = userName;
+  })
 }
